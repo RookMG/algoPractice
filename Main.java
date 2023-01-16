53c2e0c7545974e47e39dee4370a08a129231f09
tngks1995 <tngks1995@naver.com>
2023-01-17 08:43:43 +0900

    first commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Main{
+
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        char[] line = br.readLine().toCharArray();
+        Deque<Integer> nums = new LinkedList<>();
+        Deque<Character> calcs = new LinkedList<>();
+        sb.append(line[0]);
+        for(int i=1;i<line.length;i++){
+            if(String.in)
+        }
+        System.out.println(sb);
+    }
+    public static void main(String[] args) throws Exception{
+        new Main().solution();
+    }
+}
